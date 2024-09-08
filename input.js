31f67678b1731fac4479fb262c5135673790ab78
PrudhviRamaRayudu <kotaprudhvi14@gmail.com>
2024-09-08 06:59:36 +0530

    Add files via upload

---
+++ input.js
@@ -0,0 +1,17 @@
+function rollDice(){
+  const numOfDice = document.getElementById("numOfDice").value;
+  const diceResults = document.getElementById("diceResults")
+  const diceImages = document.getElementById("diceImages");
+  const values=[];
+  const images=[];
+
+for (let i=0 ;i < numOfDice;i++){
+
+  const value = Math.floor(Math.random()*6)+1;
+  console.log(value);
+  values.push(value)
+  images.push(`<img src="dice/${value}.png">` );
+  }
+  diceResults.textContent =`dice:${values.join(',')}`;
+  diceImages.innerHTML = images.join('');
+} 
